d1a93976742079c50e0b24ab575fee1a05bddbdc
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2021-01-24 17:41:40 +0100

    First connection to TFS (#5)
    * Add FileNew projects
    
    * First version that talkes to TFS
    
    * CleanUp

---
+++ Core/TFSUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.VisualStudio.Services.Common;
+
+namespace Core
+{
+    public class TfsUtil
+    {
+        private readonly VersionControlServer _vcServer;
+
+        public TfsUtil(string url)
+        {
+            var tfs = new TfsTeamProjectCollection(new Uri(url), new VssCredentials());
+            _vcServer = tfs.GetService<VersionControlServer>();
+        }
+
+        //VssConnection vss 
+        public int[] GetHistory(string path)
+        {
+            var z = _vcServer.QueryHistory(path, RecursionType.Full).ToArray();
+            
+
+
+            return z.Select(c => c.ChangesetId).ToArray();
+        }
+
+    }
+}
---
+++ TfvcToGitTool/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Core;
+
+namespace TfvcToGitTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tfsUrl = args[0];
+            string path = args[1];
+
+            var tfs = new TfsUtil(tfsUrl);
+
+
+            var z = tfs.GetHistory(path);
+
+        }
+    }
+}
