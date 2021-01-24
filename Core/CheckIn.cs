85ea7118a56381b2f218d65365ec5e8d5f0d5a73
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2021-01-24 22:03:35 +0100

    First working code (#6)
    * Add FileNew projects
    
    * First version that talkes to TFS
    
    * CleanUp
    
    * Seems to work
    
    * Update Program.cs

---
+++ Core/CheckIn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using NodaTime;
+
+namespace Core
+{
+    public class CheckIn
+    {
+        public int Id;
+        public string commiterName;
+        public string committerEmail;
+        public string comment;
+        public LocalDateTime date;
+    }
+}
